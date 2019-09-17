479b76903caffff8f32bc807a14a7d979e251aa9
Omulosi <mulongojohnpaul@gmail.com>
2019-09-17 18:17:26 +0300

    Create sub navigation menus for mac and ipad menu links

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,36 +2,64 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import NavWrapper from './components/NavWrapper';
+import SubNav from './components/SubNav';
 
 
 const appleNav = [
   {
-    url: '/mac',
-    name: 'Mac'
+    url: 'mac',
+    name: 'Mac',
+    items: [
+      {name: 'MacBook Air'},
+      {name: 'MacBook Pro'},
+      {name: 'iMac'},
+      {name: 'iMac Pro'},
+      {name: 'Mac Pro'},
+      {name: 'Mac Mini'},
+      {name: 'Pro Display XDR'},
+      {name: 'Accessories'},
+      {name: 'Catalina'}
+    ]
   },
   {
-    url: '/ipad',
-    name: 'iPad'
+    url: 'ipad',
+    name: 'iPad',
+    items: [
+      {name: 'iPad Air'},
+      {name: 'iPad Pro'},
+      {name: 'iPad'},
+      {name: 'iPad Mini'},
+      {name: 'Compare'},
+      {name: 'Apple Pencil'},
+      {name: 'Smart Keyboard'},
+      {name: 'Accessories'},
+      {name: 'iPadOs'}
+    ]
   },
   {
-    url: '/iphone',
-    name: 'iPhone'
+    url: 'iphone',
+    name: 'iPhone',
+    items: []
   },
   {
-    url: '/watch',
-    name: 'Watch'
+    url: 'watch',
+    name: 'Watch',
+    items: []
   },
   {
-    url: '/tv',
-    name: 'TV'
+    url: 'tv',
+    name: 'TV',
+    items: []
   },
   {
-    url: '/music',
-    name: 'Music'
+    url: 'music',
+    name: 'Music',
+    items: []
   },
   {
-    url: '/support',
-    name: 'Support'
+    url: 'support',
+    name: 'Support',
+    items: []
   }
 ]
 
@@ -40,7 +68,7 @@ function App() {
   return (
     <div className="container">
       <NavWrapper navTitles={appleNav} />
-      <Route path="/" render={() => {
+      <Route exact path="/" render={() => {
           return (
             <div className="main-body">
               <h1>Apple Products</h1>
@@ -48,6 +76,14 @@ function App() {
           )
         }}
       />
+      
+      <Route path="/:name" render={(props) =>{
+        const name = props.match.params.name;
+        const obj = appleNav.find((nav) => {
+          return nav.url === name;
+        });
+        return <SubNav items={obj.items} />
+      } } />
     </div>
   );
 }
